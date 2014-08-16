1be58c658152c9233ff7b1279b0c2eb815904fe7
bennahugo <bennahugo@aol.com>
2014-08-16 12:58:01 +0200

    Added facility to read in multiple measurement sets and image them together. This closes issue #25

--- bullseye.py
+++ bullseye.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import argparse
+from pyparsing import commaSeparatedList
 import numpy as np
 import pylab
 import re
@@ -24,19 +25,11 @@ def coords(s):
     except:
         raise argparse.ArgumentTypeError("Coordinates must be ra,dec tupples")
 
-def measurement_names(s):
-    try:
-	sT = s.strip()
-        ms_names = sT.split(',')
-        return 
-    except:
-        raise argparse.ArgumentTypeError("Coordinates must be ra,dec tupples")
-
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Bullseye: An implementation of targetted facet-based synthesis imaging in radio astronomy.')
   pol_options = {'I' : 1, 'Q' : 2, 'U' : 3, 'V' : 4, 'RR' : 5, 'RL' : 6, 'LR' : 7, 'LL' : 8, 'XX' : 9, 'XY' : 10, 'YX' : 11, 'YY' : 12} # as per Stokes.h in casacore, the rest is left unimplemented
-  parser.add_argument('input_ms', help='Name of the measurement set to read', type=str)
-  parser.add_argument('output_prefix', help='Prefix for the output FITS images. Facets will be indexed as [prefix_1.fits ... prefix_n.fits]', type=str)
+  parser.add_argument('input_ms', help='Name of the measurement set(s) to read. Multiple MSs must be comma-delimited without any separating spaces, eg. "\'one.ms\',\'two.ms\'"', type=str)
+  parser.add_argument('--output_prefix', help='Prefix for the output FITS images. Facets will be indexed as [prefix_1.fits ... prefix_n.fits]', type=str, default='out.bullseye')
   parser.add_argument('--facet_centres', help='List of coordinate tupples indicating facet centres (RA,DEC). If none present default pointing centre will be used', type=coords, nargs='+', default=None)
   parser.add_argument('--npix_l', help='Number of facet pixels in l', type=int, default=256)
   parser.add_argument('--npix_m', help='Number of facet pixels in m', type=int, default=256)
@@ -55,59 +48,69 @@ if __name__ == "__main__":
   parser.add_argument('--output_psf',help='Outputs the point-spread-function',type=bool,default=False)
   parser.add_argument('--sample_weighting',help='Specify weighting technique in use.',choices=['natural','uniform'], default='natural')
   parser_args = vars(parser.parse_args())
-  data = data_set_loader.data_set_loader(parser_args['input_ms'],read_jones_terms=parser_args['do_jones_corrections'])
-  data.read_head()
-  chunk_size = data.compute_number_of_rows_to_read_from_mem_requirements(parser_args['mem_available_for_input_data'])
-  if chunk_size == 0:
-    raise Exception("Insufficient memory allocated for loading data. Cannot even load a single row and a timestamp of jones matricies at a time")
-  no_chunks = data.number_of_read_iterations_required_from_mem_requirements(parser_args['mem_available_for_input_data'])
-  #some sanity checks:
-  if (parser_args['pol'] in ['I','Q','U','V'] and 
+  
+  #initially the output grids must be set to NONE. Memory will only be allocated before the first MS is read.
+  gridded_vis = None
+  g = None
+  sampling_funct = None
+  
+  ms_names = commaSeparatedList.parseString(parser_args['input_ms'])
+  for ms_index,ms in enumerate(ms_names):
+    print "NOW IMAGING %s" % ms
+    data = data_set_loader.data_set_loader(ms,read_jones_terms=parser_args['do_jones_corrections'])
+    data.read_head()
+    chunk_size = data.compute_number_of_rows_to_read_from_mem_requirements(parser_args['mem_available_for_input_data'])
+    if chunk_size == 0:
+      raise Exception("Insufficient memory allocated for loading data. Cannot even load a single row and a timestamp of jones matricies at a time")
+    no_chunks = data.number_of_read_iterations_required_from_mem_requirements(parser_args['mem_available_for_input_data'])
+    #some sanity checks:
+    if (parser_args['pol'] in ['I','Q','U','V'] and 
       pol_options[parser_args['pol']] not in data._polarization_correlations and 
       data._polarization_correlations.tolist() not in [[pol_options['RR'],pol_options['RL'],pol_options['LR'],pol_options['LL']],	#circular correlation products
 						       [pol_options['XX'],pol_options['XY'],pol_options['YX'],pol_options['YY']]]):	#linear correlation products
       raise argparse.ArgumentTypeError("Unsupported polarization option specified in Measurement Set. Stokes terms "
 				       "may only be derived from ['RR','RL','LR','LL'] or ['XX','XY','YX','YY']")
   
-  elif (parser_args['pol'] not in ['I','Q','U','V']) and (pol_options[parser_args['pol']] not in data._polarization_correlations):
-    raise argparse.ArgumentTypeError("Cannot obtain requested gridded polarization from the provided measurement set.")
-  if parser_args['field_id'] not in range(0,len(data._field_centre_names)):
-    raise argparse.ArgumentTypeError("Specified field does not exist Must be in 0 ... %d for this Measurement Set" % (len(data._field_centre_names) - 1))
-  if parser_args['do_jones_corrections'] and data._no_polarization_correlations != 4:
-    raise argparse.ArgumentTypeError("Measurement set must contain 4 correlation terms per visibility in order to apply corrective jones matricies")
-  if parser_args['do_jones_corrections'] and (not data._dde_cal_info_exists or not data._dde_cal_info_desc_exists):
-    raise argparse.ArgumentTypeError("Measurement set does not contain corrective DDE terms or the description table is missing.")
+    elif (parser_args['pol'] not in ['I','Q','U','V']) and (pol_options[parser_args['pol']] not in data._polarization_correlations):
+      raise argparse.ArgumentTypeError("Cannot obtain requested gridded polarization from the provided measurement set.")
+    if parser_args['field_id'] not in range(0,len(data._field_centre_names)):
+      raise argparse.ArgumentTypeError("Specified field does not exist Must be in 0 ... %d for this Measurement Set" % (len(data._field_centre_names) - 1))
+    if parser_args['do_jones_corrections'] and data._no_polarization_correlations != 4:
+      raise argparse.ArgumentTypeError("Measurement set must contain 4 correlation terms per visibility in order to apply corrective jones matricies")
+    if parser_args['do_jones_corrections'] and (not data._dde_cal_info_exists or not data._dde_cal_info_desc_exists):
+      raise argparse.ArgumentTypeError("Measurement set does not contain corrective DDE terms or the description table is missing.")
   
-  conv = convolution_filter.convolution_filter(parser_args['conv_sup'],parser_args['conv_sup'],
-					       parser_args['conv_oversamp'],parser_args['npix_l'],
-					       parser_args['npix_m'],parser_args['conv'])
-  print "IMAGING ONLY FIELD %s" % data._field_centre_names[parser_args['field_id']]
+    conv = convolution_filter.convolution_filter(parser_args['conv_sup'],parser_args['conv_sup'],
+						 parser_args['conv_oversamp'],parser_args['npix_l'],
+						 parser_args['npix_m'],parser_args['conv'])
+    print "IMAGING ONLY FIELD %s" % data._field_centre_names[parser_args['field_id']]
   
-  #check how many facets we have to create (if none we'll do normal gridding without any transformations)
-  facet_centres = None
-  num_facet_centres = 0
-  if (parser_args['facet_centres'] != None):
-    num_facet_centres = len(parser_args['facet_centres'])
-    facet_centres = np.array(parser_args['facet_centres']).astype(base_types.uvw_type)
-  if parser_args['do_jones_corrections'] and num_facet_centres != data._cal_no_dirs:
-    raise argparse.ArgumentTypeError("Number of calibrated directions does not correspond to number of directions being faceted")
+    #check how many facets we have to create (if none we'll do normal gridding without any transformations)
+    facet_centres = None
+    num_facet_centres = 0
+    if (parser_args['facet_centres'] != None):
+      num_facet_centres = len(parser_args['facet_centres'])
+      facet_centres = np.array(parser_args['facet_centres']).astype(base_types.uvw_type)
+    if parser_args['do_jones_corrections'] and num_facet_centres != data._cal_no_dirs:
+      raise argparse.ArgumentTypeError("Number of calibrated directions does not correspond to number of directions being faceted")
   
-  #allocate enough memory to compute image and or facets
-  gridded_vis = None
-  g = None
-  sampling_funct = None
-  if pol_options[parser_args['pol']] in data._polarization_correlations.tolist() and not parser_args['do_jones_corrections']:
-	num_grids = 1 if (num_facet_centres == 0) else num_facet_centres
-	g = np.zeros([num_grids,1,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.grid_type)
-	if parser_args['output_psf'] or parser_args['sample_weighting'] != 'natural':
-	  sampling_funct = np.zeros([num_grids,1,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.psf_type)
-  else:
-	num_polarized_grids = 1 if (num_facet_centres == 0) else num_facet_centres
-	g = np.zeros([num_polarized_grids,4,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.grid_type)
-	if parser_args['output_psf'] or parser_args['sample_weighting'] != 'natural':
-	  sampling_funct = np.zeros([num_polarized_grids,1,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.psf_type) #only one Sampling function over all polarizations
+    #allocate enough memory to compute image and or facets (only before gridding the first MS)
+    if pol_options[parser_args['pol']] in data._polarization_correlations.tolist() and not parser_args['do_jones_corrections']:
+	if g == None:
+	  num_grids = 1 if (num_facet_centres == 0) else num_facet_centres
+	  g = np.zeros([num_grids,1,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.grid_type)
+	if sampling_funct == None:
+	  if parser_args['output_psf'] or parser_args['sample_weighting'] != 'natural':
+	    sampling_funct = np.zeros([num_grids,1,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.psf_type)
+    else:
+	if g == None:
+	  num_polarized_grids = 1 if (num_facet_centres == 0) else num_facet_centres
+	  g = np.zeros([num_polarized_grids,4,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.grid_type)
+	if sampling_funct == None:
+	  if parser_args['output_psf'] or parser_args['sample_weighting'] != 'natural':
+	    sampling_funct = np.zeros([num_polarized_grids,1,parser_args['npix_l'],parser_args['npix_m']],dtype=base_types.psf_type) #only one Sampling function over all polarizations
 	  
-  for chunk_index in range(0,no_chunks):
+    for chunk_index in range(0,no_chunks):
       #carve up the data in this measurement set:
       chunk_lbound = chunk_index * chunk_size
       chunk_ubound = min((chunk_index+1) * chunk_size,data._no_rows)
@@ -162,7 +165,7 @@ if __name__ == "__main__":
 	  else:
 	    libimaging.facet_single_pol(ctypes.byref(params))
 	
-	if chunk_index == no_chunks - 1:
+	if chunk_index == no_chunks - 1 and ms_index == len(ms_names) - 1:
 	  if parser_args['sample_weighting'] == 'uniform':
 	    for f in range(0,max(num_facet_centres,1)):
 	      g[f,0,:,:] /= (sampling_funct[f,:,:].reshape(parser_args['npix_l'],parser_args['npix_m']) + 0.00000001) #plus some epsilon to make sure we don't get divide by zero issues here
@@ -193,7 +196,7 @@ if __name__ == "__main__":
 	      libimaging.facet_4_cor_with_sampling_func(ctypes.byref(params))
 	    else:
 	      libimaging.facet_4_cor(ctypes.byref(params))
-	if chunk_index == no_chunks - 1:      
+	if chunk_index == no_chunks - 1 and ms_index == len(ms_names) - 1:      
 	  if parser_args['sample_weighting'] == 'uniform':
 	    for f in range(0,max(num_facet_centres,1)):
 	      for p in range(0,4):
