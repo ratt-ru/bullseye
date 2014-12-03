644c16e3c27bfa3a97fdc3be56deabbe4ac977fe
bennahugo <bennahugo@aol.com>
2014-12-03 12:25:52 +0200

    Reworked convolution filters once more and added debugging plots

--- helpers/convolution_filter.py
+++ helpers/convolution_filter.py
@@ -7,7 +7,10 @@ import numpy as np
 import math
 import fft_utils
 import base_types
-import pylab
+import matplotlib.pyplot as plt
+
+DEBUG_KERNEL_ON = False
+
 class convolution_filter(object):
   '''
   classdocs
@@ -41,11 +44,11 @@ class convolution_filter(object):
       result += ((z/2.0)**(2*k))/(math.factorial(k)**2)
     return result
 
-  def keiser_bessel(self,x,grid_max_x):
-    return self.modified_zero_order_bessel_first_kind(self.beta*math.sqrt(1-(2.0*x)**2))
+  def keiser_bessel(self,x,w):
+    return self.modified_zero_order_bessel_first_kind(self.beta*math.sqrt(1-(2.0*x/w)**2))*(1.0/w)
   
-  def keiser_bessel_fourier(self,x,grid_max_x):
-    sqrt_term_sq = (math.pi*x)**2 - self.beta**2 #assuming W = 1
+  def keiser_bessel_fourier(self,x,w):
+    sqrt_term_sq = (math.pi*w*x)**2 - self.beta**2
     sqrt_term = np.sqrt(sqrt_term_sq) if sqrt_term_sq > 0 else 0.00000001 #ignore imaginary component since the image is supposed to be real anyway
     return np.sin(sqrt_term)/sqrt_term
 
@@ -61,13 +64,18 @@ class convolution_filter(object):
     convolution_size = convolution_fir_support * oversampling_factor
     convolution_centre = convolution_size / 2
     
-    conv_FIR_dim = np.zeros([convolution_size],dtype=base_types.fir_type)
+    conv_FIR_dim = np.empty([convolution_size],dtype=base_types.fir_type)
     for x in range(0,convolution_size):
-      conv_FIR_dim[x] = convolution_func[function_to_use]((x - convolution_centre)/float(convolution_size),grid_size_l)
+      index = x
+      conv_FIR_dim[index] = convolution_func[function_to_use]((x - convolution_centre)/float(oversampling_factor),convolution_fir_support)
     
     self._conv_FIR = np.outer(conv_FIR_dim,conv_FIR_dim) # the tensor product is probably the fastest way to get this matrix out with python
     self._conv_FIR /= np.sum(self._conv_FIR) #the convolution function should integrate to unity, so that the whole image doesn't change when we change the oversampling
-    
+    if DEBUG_KERNEL_ON:
+      plt.figure()
+      plt.title("Convolution Kernel")
+      plt.imshow(self._conv_FIR)
+      plt.show(False)
     print " <DONE>"
     
     print("CREATING DETAPERER... "),
@@ -76,10 +84,16 @@ class convolution_filter(object):
 	
     F_detaper_l = np.zeros([grid_size_l],dtype=base_types.detaper_type)
     for li in range(0,grid_size_l):
-      F_detaper_l[li] = detaper_func[function_to_use]((li-detaper_centre_l)/float(detaper_centre_l),grid_size_l)
+      F_detaper_l[li] = detaper_func[function_to_use]((li-detaper_centre_l)/float(grid_size_l),convolution_fir_support)
     F_detaper_m = np.zeros([grid_size_m],dtype=base_types.detaper_type)
     for mi in range(0,grid_size_m):
-      F_detaper_m[mi] = detaper_func[function_to_use]((mi-detaper_centre_m)/float(detaper_centre_m),grid_size_m)
+      F_detaper_m[mi] = detaper_func[function_to_use]((mi-detaper_centre_m)/float(grid_size_m),convolution_fir_support)
       
     self._F_detaper = np.outer(F_detaper_l,F_detaper_m) # the tensor product is probably the fastest way to get this matrix out with python
+    if DEBUG_KERNEL_ON:
+      plt.figure()
+      plt.title("AA Taper")
+      plt.imshow(self._F_detaper)
+      plt.show(True)
+      #exit()
     print " <DONE>"
