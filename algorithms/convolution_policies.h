a223a658ed03c3a585a00404186633b2aae3d861
bennahugo <bennahugo@aol.com>
2015-01-02 20:12:25 +0200

    Switched to sinc... the filter response is actually closest to a rectangle of everything I tried and the intensities at the edge of the image is closer to anything I've achieved so far when I compare it to lwimager. The current convolution kernel halfwidth is 7. Will implement this filter generation in c++ and then hopefully I can call this fix on the image quality issue. Need to move back on to GPU acceleration along with the multiple different faceting approaches otherwise I will run out of time.

--- algorithms/convolution_policies.h
+++ algorithms/convolution_policies.h
@@ -64,30 +64,58 @@ namespace imaging {
 			_nx(nx), _ny(ny), _grid_size_in_pixels(nx*ny), _grid_u_centre(nx / 2.0), _grid_v_centre(ny / 2.0),
 			_convolution_support(convolution_support*2 + 1), _oversampling_factor(oversampling_factor), 
 			_conv(conv), _conv_dim_size(_convolution_support * _oversampling_factor),
-			_conv_centre_offset((convolution_support)/2.0),
+			_conv_centre_offset((_convolution_support)/2.0),
 			_active_gridding_policy(active_gridding_policy),
 			_cube_chan_dim_step(nx*ny*no_polarizations)
 			{}
     inline void convolve(const uvw_coord<uvw_base_type> & __restrict__ uvw,
 			 const typename gridding_policy_type::trait_type::pol_vis_type & __restrict__ vis,
 			 std::size_t no_grids_to_offset) const __restrict__ {
-	auto convolve = [this](convolution_base_type x){
-// 	  convolution_base_type beta = 4.686466667f * this->_convolution_support - 0.3422f; //regression line
-// 	  convolution_base_type sqr_term = (2 * x / this->_convolution_support);
-// 	  convolution_base_type sqrt_term = 1 - sqr_term * sqr_term;
-	  /*funciton is only defined within -support*0.5 <= x <= support*0.5
-	   *introduce a boxcar to ensure we dont fall off that support region
-	   *this means however that we're tapering with a sinc function in the intensity domain
-	   */ 
-// 	  if (sqrt_term < 0)
-// 	    return (convolution_base_type)0.0;
-// 	  return (convolution_base_type)boost::math::cyl_bessel_j<convolution_base_type>(0,beta * sqrt(sqrt_term))/this->_convolution_support;
-	  //This crap introduces a stuff load of radial tapering in the intensity domain... only the kaiser bessel really work as advertised:
-// 	  #define ALPHA 0.4463
-// 	  return ALPHA + (1 - ALPHA) * cos(2*M_PI*x/this->_convolution_support);
-	  convolution_base_type sigma =  0.0349*x + 0.37175;
-	  return exp(-0.5 * (x/sigma)*(x/sigma));
+	auto bessel = [](convolution_base_type x){
+	  #define EPSILON 0.0000001
+	  convolution_base_type res = 0;
+	  convolution_base_type old = -10;
+	  int m = 0;
+	  while (abs(res - old) > EPSILON) {
+	    old = res;
+	    res += pow(-0.25 * x * x,m) / pow(boost::math::factorial<convolution_base_type>(m),2);
+	    ++m;
+	  }
+	  return res;
 	};
+	auto convolve = [this,&bessel](convolution_base_type x){
+// 	  convolution_base_type beta = 1.6789f * (this->_convolution_support+1) - 0.9644; //regression line
+// 	  convolution_base_type sqr_term = (2 * x / (this->_convolution_support+1));
+// 	  convolution_base_type sqrt_term = 1 - sqr_term * sqr_term;
+// 	  return (convolution_base_type)boost::math::cyl_bessel_j<convolution_base_type>(0,beta * sqrt(sqrt_term))/(this->_convolution_support+1);
+
+	  //two element cosine (this is a complete stuffup...)
+// 	  convolution_base_type alpha = -0.1307 * (this->_convolution_support+1) + 0.9561;
+// 	  return alpha + (1 - alpha) * cos(2*M_PI*x/(this->_convolution_support+1));
+
+	  //gausian introduces tapering
+// 	  convolution_base_type sigma =  0.0349* this->_convolution_support + 0.37175;
+// 	  return exp(-0.5 * (x/sigma)*(x/sigma));
+	  
+	  //sinc works okay
+	  convolution_base_type param = M_PI*(x+0.00000001);
+	  return sin(param) / param;
+	};
+	
+	static bool output_filter = true;
+	#include <string.h>
+	if (output_filter){
+	  FILE * pFile;
+	  pFile = fopen("/scratch/filter.txt","w");
+	  for (int x = -_convolution_support*_oversampling_factor/2; x <= _convolution_support*_oversampling_factor/2; ++x){
+	    fprintf(pFile,"%f",convolve(x/(float)_oversampling_factor));
+	    if (x < _convolution_support*_oversampling_factor/2)
+	      fprintf(pFile,",");
+	  }
+	  fclose(pFile);
+	  output_filter = false;
+	}
+	
 	std::size_t chan_offset = no_grids_to_offset * _cube_chan_dim_step;
 
 	uvw_base_type translated_grid_u = uvw._u + _grid_u_centre - _conv_centre_offset;
@@ -95,15 +123,16 @@ namespace imaging {
 	std::size_t disc_grid_u = std::lrint(translated_grid_u);
 	std::size_t disc_grid_v = std::lrint(translated_grid_v);
 	
-	if (disc_grid_v + _convolution_support  >= _ny || disc_grid_u + _convolution_support  >= _nx) return;
+	if (disc_grid_v + _convolution_support/2  >= _ny || disc_grid_u + _convolution_support/2  >= _nx) return;
+	
 	{
-	    uvw_base_type frac_u = (-translated_grid_u + (uvw_base_type)disc_grid_u);
-	    uvw_base_type frac_v = (-translated_grid_v + (uvw_base_type)disc_grid_v);
+	    uvw_base_type frac_u = -translated_grid_u + (uvw_base_type)disc_grid_u;
+	    uvw_base_type frac_v = -translated_grid_v + (uvw_base_type)disc_grid_v;
 	    
             for (std::size_t sup_v = 0; sup_v < _convolution_support; ++sup_v) {
                 std::size_t convolved_grid_v = disc_grid_v + sup_v;
                 uvw_base_type conv_v = (uvw_base_type)sup_v - _conv_centre_offset + frac_v;
-                for (int sup_u = 0; sup_u < _convolution_support; ++sup_u) {
+                for (std::size_t sup_u = 0; sup_u < _convolution_support; ++sup_u) {
                     std::size_t convolved_grid_u = disc_grid_u + sup_u;
 		    uvw_base_type conv_u = (uvw_base_type)sup_u - _conv_centre_offset + frac_u;
                     std::size_t grid_flat_index = convolved_grid_v*_ny + convolved_grid_u;
@@ -113,23 +142,6 @@ namespace imaging {
                 }
             }
 	}
-// 	{
-// 	    uvw_base_type frac_u = (translated_grid_u - (uvw_base_type)disc_grid_u) + 1;
-// 	    uvw_base_type frac_v = (translated_grid_v - (uvw_base_type)disc_grid_v) + 1;
-// 	    
-//             for (std::size_t sup_v = 0; sup_v <= _convolution_support/2; ++sup_v) {
-//                 std::size_t convolved_grid_v = disc_grid_v + sup_v + 1;
-//                 uvw_base_type conv_v = (uvw_base_type)sup_v - _conv_centre_offset + frac_v;
-//                 for (int sup_u = 0; sup_u <= _convolution_support/2; ++sup_u) {
-//                     std::size_t convolved_grid_u = disc_grid_u + sup_u + 1;
-// 		    uvw_base_type conv_u = (uvw_base_type)sup_u - _conv_centre_offset + frac_u;
-//                     std::size_t grid_flat_index = convolved_grid_v*_ny + convolved_grid_u;
-// 
-//                     convolution_base_type conv_weight = convolve(conv_v) * convolve(conv_u);
-//                     _active_gridding_policy.grid_polarization_terms(chan_offset + grid_flat_index, vis, conv_weight);
-//                 }
-//             }
-// 	}
     }
   };
 }
