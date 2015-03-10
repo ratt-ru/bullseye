4de09c143865a4c1479992acf33866df0ecb0747
Benjamin <bennahugo@aol.com>
2015-03-10 17:46:21 +0200

    woops forgot to add the long lists of new files... note that base_types.h is now the keeper of the typedefs

---
+++ algorithms/base_types.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef BULLSEYE_SINGLE
+  typedef float visibility_base_type;
+  typedef float uvw_base_type;
+  typedef float reference_wavelengths_base_type;
+  typedef float convolution_base_type;
+  typedef float visibility_weights_base_type;
+  typedef float grid_base_type;
+  #define SHOULD_DO_32_BIT_FFT
+#endif
+#ifdef BULLSEYE_DOUBLE
+  typedef double visibility_base_type;
+  typedef double uvw_base_type;
+  typedef double reference_wavelengths_base_type;
+  typedef double convolution_base_type;
+  typedef double visibility_weights_base_type;
+  typedef double grid_base_type;
+#endif
