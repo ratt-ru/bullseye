39f85fe74616e957e22c2c5a21f2ea7bea1510a8
Simon Perkins <simon.perkins@gmail.com>
2015-03-25 16:34:24 +0200

    Use a MANIFEST.in to include the shared libraries. Add an __init__.py so that bullseye is viewed as a package.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def bullseye_pkg_dirs():
     """
     pkg_dirs = []
 
-    print '-'*80, '\n'
+    #print '-'*80, '\n'
 
     path = pjoin(pkg, 'cbuild', 'algorithms')
     # Ignore
@@ -23,9 +23,9 @@ def bullseye_pkg_dirs():
 
     # Walk 'bullseye/src'
     for root, dirs, files in os.walk(path, topdown=True):
-        print '-'*20, 'ROOTS %s' % root
-        print '-'*20, 'DIRS %s' % dirs
-        print '-'*20, 'FILES %s' % files
+        #print '-'*20, 'ROOTS %s' % root
+        #print '-'*20, 'DIRS %s' % dirs
+        #print '-'*20, 'FILES %s' % files
 
         # Prune out everything we're not interested in
         # from os.walk's next yield.
@@ -35,7 +35,7 @@ def bullseye_pkg_dirs():
             pkg_dirs.append(pjoin(root, d, '*.*'))
 
 
-    print 'pkgdirs %s' % pkg_dirs
+    #print 'pkgdirs %s' % pkg_dirs
 
     return pkg_dirs
 
@@ -63,8 +63,6 @@ setup(name=pkg,
     install_requires=[
         'numpy',
         'pyfits'],
-    package_data={pkg : bullseye_pkg_dirs()},
+    #package_data={pkg : bullseye_pkg_dirs()},
     include_package_data=True,
     zip_safe=False)
-
-print bullseye_pkg_dirs()
