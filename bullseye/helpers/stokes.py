2957424a9b5edee1cf522dcf8c96b49d69668e3a
bennahugo <bennahugo@aol.com>
2015-03-28 11:32:50 +0200

    Added GNU GPL 2 preamble to start of each file in the repo (#55)

--- bullseye/helpers/stokes.py
+++ bullseye/helpers/stokes.py
@@ -1,3 +1,42 @@
+'''
+Bullseye:
+An accelerated targeted facet imager
+Category: Radio Astronomy / Widefield synthesis imaging
+
+Authors: Benjamin Hugo, Oleg Smirnov, Cyril Tasse, James Gain
+Contact: hgxben001@myuct.ac.za
+
+Copyright (C) 2014-2015 Rhodes Centre for Radio Astronomy Techniques and Technologies
+Department of Physics and Electronics
+Rhodes University
+Artillery Road P O Box 94
+Grahamstown
+6140
+Eastern Cape South Africa
+
+Copyright (C) 2014-2015 Department of Computer Science
+University of Cape Town
+18 University Avenue
+University of Cape Town
+Rondebosch
+Cape Town
+South Africa
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+'''
+
 pol_options = {'I' : 1, 'Q' : 2, 'U' : 3, 'V' : 4, 'RR' : 5, 'RL' : 6, 'LR' : 7, 'LL' : 8, 'XX' : 9, 'XY' : 10, 'YX' : 11, 'YY' : 12} # as per Stokes.h in casacore, the rest is left unimplemented
 '''
 See Smirnov I (2011) for description on conversion between correlation terms and stokes params for linearly polarized feeds
--- bullseye_mo/gpu_algorithm/phase_transform_policies.h
+++ bullseye_mo/gpu_algorithm/phase_transform_policies.h
@@ -1,3 +1,41 @@
+/********************************************************************************************
+Bullseye:
+An accelerated targeted facet imager
+Category: Radio Astronomy / Widefield synthesis imaging
+
+Authors: Benjamin Hugo, Oleg Smirnov, Cyril Tasse, James Gain
+Contact: hgxben001@myuct.ac.za
+
+Copyright (C) 2014-2015 Rhodes Centre for Radio Astronomy Techniques and Technologies
+Department of Physics and Electronics
+Rhodes University
+Artillery Road P O Box 94
+Grahamstown
+6140
+Eastern Cape South Africa
+
+Copyright (C) 2014-2015 Department of Computer Science
+University of Cape Town
+18 University Avenue
+University of Cape Town
+Rondebosch
+Cape Town
+South Africa
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+********************************************************************************************/
 #pragma once
 #include "gridding_parameters.h"
 #include "cu_common.h"
